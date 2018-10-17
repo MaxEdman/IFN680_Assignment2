ee1daba31c12b016da81495e3465c0235ffa79a9
Max Edman <max.edman@gmail.com>
2018-10-17 22:15:37 +1000

    Dataset split is done

--- .ipynb_checkpoints/my_submission-checkpoint.py
+++ .ipynb_checkpoints/my_submission-checkpoint.py
@@ -1,21 +1,70 @@
 
 import numpy as np
-from tensorflow.contrib import keras
 
-def main_tester():
+# Defining function to save the dataset to be used in Assignment #2 in Unit IFN680 @ QUT
+def save_mnist_dataset():
+    from tensorflow.contrib import keras
     from keras.datasets import mnist
+    
+    # Loads the dataset.
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    print("x_train")
-    print(x_train)
-    print("y_train")
-    print(y_train)
-    print("x_test")
-    print(x_test)
-    print("y_test")
-    print(y_test)
-    return
+    
+    # Saves the dataset locally as a .npz file.
+    np.savez('mnist_dataset.npz',
+             x_train = x_train,
+             y_train = y_train,
+             x_test = x_test,
+             y_test = y_test)
 
-
-
+# Function to load the mnist dataset    
+def load_mnist_dataset():
+    # Loads the dataset from the locally saved .npz file.
+    with np.load('mnist_dataset.npz') as npzfile:
+        x_train = npzfile['x_train']
+        y_train = npzfile['y_train']
+        x_test = npzfile['x_test']
+        y_test = npzfile['y_test']
+    
+    return (x_train, y_train), (x_test, y_test)
+    
+# Function to implement the preprocessing steps of:
+#   Split the dataset such that ...
+#   ◦ the digits in [2,3,4,5,6,7] are used for training and testing
+#   ◦ the digits in [0,1,8,9] are only used for testing. None of these digits should be used during training.
+def preprocess_mnist_dataset(x_train, y_train, x_test, y_test):
+    
+    # Using numpy module to concatenate train and test data into their own datasets. Respectively for x and y
+    image_dataset = np.concatenate([x_train, x_test])
+    target_dataset = np.concatenate([y_train, y_test])
+    
+    # Creates a mask with all numbers that should be used for both training and testing. I.e. where target dataset is any of the following numbers [2,3,4,5,6,7]. Will be further called tnt_dataset
+    train_and_test_mask = np.logical_and(target_dataset>1, target_dataset<8)
+    #print("train_and_test_mask shape: {0}", train_and_test_mask.shape)
+    #print("image_dataset shape: {0}", image_dataset.shape)
+    
+    # Initiating two new arrays with data and target based on previous created mask. These will be used for both testing and traning.
+    #tnt_dataset = np.ma.array(image_dataset, mask=train_and_test_mask)
+    tnt_dataset = image_dataset[train_and_test_mask,:,:]
+    #print("tnt_dataset shape: {0}", tnt_dataset.shape)
+    tnt_target = target_dataset[train_and_test_mask]
+    #print("tnt_target shape: {0}", tnt_target.shape)
+    
+    # Initiating two new arrays with data and target based on the remaining entries in the array. I.e. the negative of the mask. These will ONLY be used for testing.
+    only_test_dataset = image_dataset[~train_and_test_mask,:,:]
+    #print("only_test_dataset shape: {0}", only_test_dataset.shape)
+    only_test_target = target_dataset[~train_and_test_mask]
+    #print("only_test_target shape: {0}", only_test_target.shape)
+    
+    # Import module for splitting datasets.
+    from sklearn.model_selection import train_test_split
+    # Splits the dataset that are used for both train and test into respective sets. With test size of 20% as stated in the implementation hints "Keep 80% of the [2,3,4,5,6,7] digits for training (and 20% for testing)."
+    data_train, data_test, target_train, target_test = train_test_split(tnt_dataset, tnt_target, test_size=0.20)
+    
+    # Concatenates the data that should be used for testing.
+    final_test_data = np.concatenate([data_test, only_test_dataset])
+    final_test_target = np.concatenate([target_test, only_test_target])
+    
+    # Returns the data to be used in training (80% of the digits [2,3,4,5,6,7]) and testing (the rest).
+    return data_train, target_train, final_test_data, final_test_target
 
 
